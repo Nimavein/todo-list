0a9d125668082b04ef4d049ab48a577e893de207
Nimavein <michalzygiel00@gmail.com>
2021-06-08 10:23:59 +0200

    create todo function add

--- src/context.tsx
+++ src/context.tsx
@@ -1,10 +1,40 @@
 import React, { useContext, useState } from "react";
 
 const AppContext = React.createContext<ContextType>({} as ContextType);
+
 const AppProvider: React.FC = ({ children }) => {
-  const [todos, setTodos] = useState<ITodo[]>([]);
+  const [categories, setCategories] = useState<Category[]>([
+    {
+      id: 1,
+      name: "first category",
+    },
+  ]);
+  const [todos, setTodos] = useState<ITodo[]>([
+    {
+      id: 1,
+      title: "post 1",
+      description: "this is my first description",
+      status: false,
+      priority: "low",
+      category: categories[0],
+    },
+  ]);
+
+  const createTodo = (todo: ITodo) => {
+    const newTodo: ITodo = {
+      id: Math.random(),
+      title: todo.title,
+      description: todo.description,
+      status: false,
+      priority: todo.priority,
+      category: todo.category,
+    };
+    setTodos([...todos, newTodo]);
+  };
   return (
-    <AppContext.Provider value={{ todos }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ todos, categories, createTodo }}>
+      {children}
+    </AppContext.Provider>
   );
 };
 
--- src/type.d.ts
+++ src/type.d.ts
@@ -2,10 +2,21 @@ interface ITodo {
   id: number;
   title: string;
   status: boolean;
+  priority: "low" | "medium" | "high";
+
+  category: Category;
+  description: string;
 }
 
 type ContextType = {
   todos: ITodo[];
-  //saveTodo: (todo: ITodo) => void;
+  categories: Category[];
+  createTodo: (todo: ITodo) => void;
   //updateTodo: (id: number) => void;
+  //deleteTodo: (id: number) => void;
 };
+
+interface Category {
+  id: number;
+  name: string;
+}
