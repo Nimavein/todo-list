7e600853ecda16d17f3da9593db48d5df2487a4b
Nimavein <michalzygiel00@gmail.com>
2021-06-14 19:33:41 +0200

    edit todo styling add

--- src/TodoEditForm.tsx
+++ src/TodoEditForm.tsx
@@ -15,6 +15,7 @@ const TodoEditForm: React.FC<TodosEditFormProps> = ({
 
   const handleForm = (e: React.FormEvent<HTMLInputElement> | any): void => {
     setFormData({
+      priority: "low",
       ...formData,
       [e.currentTarget.id]: e.currentTarget.value,
     });
@@ -27,25 +28,54 @@ const TodoEditForm: React.FC<TodosEditFormProps> = ({
     handleEditVisibility();
   };
   return (
-    <div>
+    <div className="todo-form-backdrop">
       <form className="todo-form" onSubmit={(e) => handleEditTodo(e, formData)}>
-        <label>
-          Title:
-          <input onChange={handleForm} required type="text" id="title" />
-        </label>
-        <label>
-          Description:
-          <input onChange={handleForm} required type="text" id="description" />
-        </label>
-        <label>
-          Priority:
-          <select onChange={handleForm} required id="priority">
-            <option value="low">low</option>
-            <option value="medium">medium</option>
-            <option value="high">high</option>
-          </select>
-        </label>
-        <button>Edit Todo</button>
+        <div className="todo-form-inputs">
+          <label className="title-label">
+            Title:
+            <input
+              className="title-input"
+              onChange={handleForm}
+              required
+              type="text"
+              id="title"
+            />
+          </label>
+          <label className="description-label">
+            Description:
+            <input
+              className="description-input"
+              onChange={handleForm}
+              required
+              type="text"
+              id="description"
+            />
+          </label>
+          <label className="priority-label">
+            Priority:
+            <select
+              className="priority-input"
+              onChange={handleForm}
+              required
+              id="priority"
+            >
+              <option value="low">low</option>
+              <option value="medium">medium</option>
+              <option value="high">high</option>
+            </select>
+          </label>
+        </div>
+
+        <div className="todo-form-buttons">
+          <button className="add-todo-form-button">Edit</button>
+          <button
+            className="cancel-todo-button"
+            onClick={() => handleEditVisibility()}
+            type="button"
+          >
+            Cancel
+          </button>
+        </div>
       </form>
     </div>
   );
